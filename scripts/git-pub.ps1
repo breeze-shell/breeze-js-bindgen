3498349696023f3da544829f55c99ccc518c5792
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2026-03-29 03:04:38 +0800

    Update git-pub.ps1

--- scripts/git-pub.ps1
+++ scripts/git-pub.ps1
@@ -1,4 +1,6 @@
 $version = node -p "require('./package.json').version"
+git add package.json
+git commit -m "chore: release v$version"
 git tag "v$version"
 git push origin "v$version"
 git push --tags
