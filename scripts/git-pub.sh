8fb54b5722cb56be606e4469899078cefa87d426
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2026-03-30 17:08:46 +0800

    fix

--- scripts/git-pub.sh
+++ scripts/git-pub.sh
@@ -1,2 +1,6 @@
-git tag v$(node -p "require('./package.json').version")
+version=$(node -p "require('./package.json').version")
+git add package.json
+git commit -m "chore: release v$version"
+git tag "v$version"
+git push origin "v$version"
 git push --tags
